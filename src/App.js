8b9273fd71890b120dc5549b0fc2aa6269d73d80
Renata Inojosa <inojosa.renata@gmail.com>
2022-09-08 18:26:53 -0300

    estilo da estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import menuImg from './justify.svg'
+import profileImg from './person-circle.svg'
 
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        <div className='menu-sidebar'><img src={menuImg} alt='menu-sidebar' /></div>
+        <div className='title'><h1>MovieFlix</h1></div>
+        <div className='profile-img'><img src={profileImg} alt='profile'/></div>
+        <div className='login'><h2>Login</h2></div>
+      </nav>
+        <h4>Últimos lançamentos</h4>
+        <hr></hr>
+        <p>lista de filmes - GET API</p>
     </div>
   );
 }
