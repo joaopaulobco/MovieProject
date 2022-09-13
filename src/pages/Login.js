9aa2506126406c460bbcd1fe88ba6a55e2a43100
Renata Inojosa <inojosa.renata@gmail.com>
2022-09-13 17:04:18 -0300

    feita página para área do login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,20 @@
 import './Login.css'
 
 const Login = () => {
+   
   return (
-    <div>Login</div>
+    <div className='login'>
+        <h2>Sign in to Movies&Series</h2>
+        <form className='formulario'>
+            <label>Username:</label>
+            <input type="text" placeholder='@username'></input>
+            <button type='submit'>Submit</button>
+        </form>
+        <div className='register'>
+            <p>Don't you have a username?</p>
+            <button type='submit'>Click here to register</button>
+        </div>
+    </div>
   )
 }
 
