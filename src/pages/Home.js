a9b9752593712310fa79b61ab8fd8d6ec66fd846
Renata Inojosa <inojosa.renata@gmail.com>
2022-09-17 13:37:51 -0300

    solved

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import './Home.css'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [ movies, setMovies ] = useState([]);
