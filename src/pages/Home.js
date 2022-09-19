0e24baaa30aef2f4d720c9e36f60b8c612a1ce63
Joao Paulo <joaopaulo.bco@gmail.com>
2022-09-19 15:15:27 -0300

    pagination e movielist criadas e adicionadas no userprofile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import './Home.css'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
+import MovieList from '../components/MovieList';
 
 const Home = () => {
     const [ movies, setMovies ] = useState([]);
@@ -16,21 +17,7 @@ const Home = () => {
     }, []);
 
     return (
-        <div className='title-movies'>
-            <h2>Movies - NOW PLAYING:</h2>
-            <div className='home'>
-            {movies.map((movie) => {
-                return (
-                    <Link to={`/movie/${movie.id}`} key={movie.id}>
-                    <div className="nowplaying-movies">
-                         <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt='poster'/>
-                        <h4>{movie.title}</h4>
-                    </div>
-                        </Link>
-                )
-            })}
-            </div>
-        </div>
+        <MovieList movies={movies}/>
     )
 }
 export default Home;  
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,42 +1,39 @@
-import './UserProfile.css'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import "./UserProfile.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Pagination from "../components/Pagination";
+import MovieList from "../components/MovieList";
 
 const UserProfile = () => {
-    const [ movies, setMovies ] = useState([]);
-    const [ pages, setPages] = useState();
-    
-    useEffect(() => {
-        axios
-            .get('https://api.themoviedb.org/3/movie/popular?api_key=e9658f468ef45fbe4c5ab164e45527bf&language=en-US&page=3')
-            .then((response) => {
-                setMovies(response.data.results)
-            })
-            .catch((error) => console.log(error));
-    }, []);
+  const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(2);
+  const [totalPages, setTotalPages] = useState(1)
 
-    return(
-<div className='welcome'>
-    <div>
-    Bem vindo, úsuario
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=e9658f468ef45fbe4c5ab164e45527bf&language=en-US&page=${currentPage}`
+      )
+      .then((response) => {
+        setMovies(response.data.results);
+        setTotalPages(response.data.total_pages);
+      })
+      .catch((error) => console.log(error));
+  }, [currentPage]);
+
+
+  return (
+    <div className="welcome">
+      <div>
+        <MovieList movies={movies} />
+        <Pagination
+          totalPages={totalPages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+      </div>
     </div>
-    <div className='container'>
-        {movies.map((movie)=>{
-            return(
-                <div>
-                <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt='poster'/>
-                
-                </div>
-
-            ) 
-
-            
-        })}
-    </div>
-    
-</div>
-    )
-}
+  );
+};
 
 export default UserProfile;
