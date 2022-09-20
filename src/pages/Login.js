efb5aa1ca9e6094a7b35df2589b0ebfdfd07206f
Renata Inojosa <inojosa.renata@gmail.com>
2022-09-19 23:21:13 -0300

    ainda nao reconhece o usuario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,50 @@
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [usersAPI, setUsersAPI] = useState([]); 
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate(); 
+
+  useEffect(() => {
+    axios
+    .get('https://ironrest.herokuapp.com/movieprojectusers')
+    .then((response) => {
+      setUsersAPI(response.data)
+    })
+    .catch((error) => window.alert('Error!'))
+  }, []); 
+  console.log(usersAPI)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+  
+  const findUser = () => {
+    usersAPI.find((user) => user.username ? navigate('/profile') : "User not found")
+  };
+
+  console.log(username)
 
   return (
     <div className='login'>
         <h2>Sign in to Movies&Series</h2>
-        <form className='formulario'>
+        <form className='formulario' onSubmit={handleSubmit}>
             <label>Username:</label>
-            <input type="text" placeholder='@username' autoFocus required></input>
-            <button type='submit'>Submit</button>
+            <input 
+              type="text" 
+              placeholder='@username' 
+              autoFocus 
+              required 
+              onChange={(event) => setUsername(event.target.value)}  
+              value={username}
+              />
+            <button type='submit' onClick={findUser}>Submit</button>
         </form>
         <div className='register'>
             <p>Don't you have a username?</p>
