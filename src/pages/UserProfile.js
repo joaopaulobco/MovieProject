4fae3aa00efc4efcf57520c7dac7d1d2e3334988
Renata Inojosa <inojosa.renata@gmail.com>
2022-09-23 20:41:20 -0300

    mobile first pronto

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -26,12 +26,9 @@ const UserProfile = () => {
   return (
     <div className="welcome">
       <div className="userpage">
-        <h1 className="hi">Hi <img src={smile} alt="smile-icon" /></h1>
-        <p>Do you wanna have your own favorite movies list?</p>
-        <p>So, it's easy! Choose the movies below and click on the button to add or remove them to your list!</p>
-        <input type='text'></input>
-        <button>search</button>
-        <h3 className="list">MOVIE LIST:</h3>
+        {/* <input type='text'></input>
+        <button>search</button> */}
+        <h3 className="list">POPULAR MOVIES:</h3>
         <MovieList movies={movies} />
         <Pagination
           totalPages={totalPages}
